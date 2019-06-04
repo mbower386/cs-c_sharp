228542f34edc1c43c5473e3d9f7117baed594b31
mbower386 <mbower386@gmail.com>
2019-06-03 19:49:14 -0700

    debugger quiz

---
+++ VSCodeDebugger/VSCodeDebugger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VSCodeDebugger
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            OuterFunction ();
+        }
+
+        public static void OuterFunction ()
+        {
+            Console.WriteLine ("Outer function code here");
+            InnerFunction ();
+        }
+
+        public static void InnerFunction ()
+        {
+            Console.WriteLine ("Inner function code here");
+        }
+    }
+}
